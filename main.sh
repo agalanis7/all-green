2527d7eb10f038d84571bbee6bfadb5ba2ce4366
agalanis7 <root@hellos-MacBook-Pro.local>
2018-09-03 12:00:00 -0400

    commit20180903

--- main.sh
+++ main.sh
@@ -13,13 +13,19 @@ read END_DATE
 DATE=$BEGIN_DATE
 
 while [[ "$DATE" -le "$END_DATE" ]]; do
-    echo 'Dont ask a barber if you need a haircut!' >> junk
+    echo 'a' >> junk
 
     git add .
 
     msg="commit${DATE}"
 
-    GIT_COMMITTER_DATE="${DATE}T12:00:00" git commit -m "$msg"
+    export GIT_AUTHOR_DATE="${DATE}T12:00:00"
+    export GIT_COMMITTER_DATE="${DATE}T12:00:00"
+    
+    git commit -m "$msg"
+
+    unset GIT_AUTHOR_DATE
+    unset GIT_COMMITTER_DATE
 
     DATE=$(date +%Y%m%d -d "$DATE + 1 day")
 
